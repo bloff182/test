da82d6b015c0db068ffd17234d1d36097372b3fa
Grzegorz Traczyk <bloff182>
2021-09-21 12:40:30 +0200

    updated controller for test

--- src/main/java/com/bloff/demoWebAppOk/api/ControllerDemoApp.java
+++ src/main/java/com/bloff/demoWebAppOk/api/ControllerDemoApp.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class ControllerDemoApp {
-	
+	// test
 	@GetMapping("/demo")
 	public String sayOk() {
 		return "OK";
